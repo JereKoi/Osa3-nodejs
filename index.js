34dfa2319fd1d413b8f1925928fdb67a88c7abcf
Jere <jerekoivisto7@gmail.com>
2024-03-09 17:48:43 +0200

    3.2-3.3

--- index.js
+++ index.js
@@ -4,15 +4,28 @@ const app = express();
 const PORT = 3001;
 
 const persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456"
-  }
+  { id: 1, name: "Arto Hellas", number: "040-123456" },
+  { id: 2, name: "John Doe", number: "050-987654" }
 ];
 
 app.get('/api/persons', (req, res) => {
   res.json(persons);
+});
+
+app.get('/info', (req, res) => {
+  const requestTime = new Date();
+  const numberOfPersons = persons.length;
+  res.send(`<p>Phonebook has info for ${numberOfPersons} people</p><p>${requestTime}</p>`);
+});
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find(person => person.id === id);
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
 });
 
 app.listen(PORT, () => {
